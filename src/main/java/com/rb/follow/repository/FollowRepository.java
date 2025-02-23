66bd23fa272066bbd47fddde6b2e0c62293c3646
RealBoy <nallabothulanagarjuna0@gmail.com>
2025-02-23 20:19:26 +0530

    changed to postgre sql

--- src/main/java/com/rb/follow/repository/FollowRepository.java
+++ src/main/java/com/rb/follow/repository/FollowRepository.java
@@ -1,7 +1,7 @@
 package com.rb.follow.repository;
 
 import com.rb.follow.entity.Follow;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface FollowRepository extends MongoRepository<Follow,String> {
+public interface FollowRepository extends JpaRepository<Follow,String> {
     Optional<Follow> findByFollowerIdAndFollowingId(String followerId,String followingId);
     @Transactional
     long deleteByFollowerIdAndFollowingId(String followerId,String followingId);
