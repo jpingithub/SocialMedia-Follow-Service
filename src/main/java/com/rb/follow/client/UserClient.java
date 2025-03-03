0c15adbd097a16c89c3a42a5e1c3c2d6bff7e705
jpingithub <nallabothulanagarjun0@gmail.com>
2025-03-03 13:04:49 +0530

    Made it ready to deploye

--- src/main/java/com/rb/follow/client/UserClient.java
+++ src/main/java/com/rb/follow/client/UserClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "USER-MANAGEMENT-SERVICE")
+@FeignClient(name = "USER-MANAGEMENT-SERVICE", url = "${usermanagement-url}")
 public interface UserClient {
     @GetMapping("/api/v1/users/search")
     ResponseEntity<User> searchUser(@RequestParam("username") String username);
